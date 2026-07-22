71bd3f695b9561bd36359ba0fca97f38b4e45d06
KingDayveed357 <daveaniago@gmail.com>
2026-07-22 13:05:10 +0100

    problems: maximum_distance_in_arrays, intersection of multiple arrays

--- Two pointers/344.reverse_string.py
+++ Two pointers/344.reverse_string.py
@@ -4,7 +4,7 @@ class Solution:
         left, right = 0, len(s) - 1
 
         while left < right:
-            s[left], s[right] = s[left], s[right]
+            s[left], s[right] = s[right], s[left]
 
             left += 1
             right -= 1
